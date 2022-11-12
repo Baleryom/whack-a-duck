5ecbfbb2d26182ee660a6c41a0df547c04114340
andreiserbanrazvan <andreiserbanrazvan@gmail.com>
2022-11-12 12:30:20 +0200

    duck gets triggered gradully

--- main.lua
+++ main.lua
@@ -1,11 +1,28 @@
+x = 0
+y = 0
 function love.load()
     image = love.graphics.newImage('image.png')
 end
 
 function love.update(dt)
+    if x < 20 then
+        x = x+1 + (15 * dt)
+    end
+    if y < 20 then
+        y = y+1 +(15 * dt)
+    end
+    x = x+1
+    y = y+1
+    if x > 800 then
+        x = 0
+    end
+
+    if y > 600 then
+        y = 0
+    end
 
 end
 
 function love.draw()
-    love.graphics.draw(image, love.math.random(0, 800), love.math.random(0, 600))
+    love.graphics.draw(image, love.math.random(0, x), love.math.random(0, y))
 end
