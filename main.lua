70bb4ba5924540e71a4f4ce6bcbd3a1faec2df6a
andreiserbanrazvan <andreiserbanrazvan@gmail.com>
2022-11-12 16:11:41 +0200

    added different delta time implementations

--- main.lua
+++ main.lua
@@ -6,28 +6,32 @@ _G.y = 0
 _G.paused = false
 local duckNumber = 0
 function love.load()
+    frame = 0
+    love.window.setMode(800, 600, { resizable = true, vsync = 1, minheight = 300 })
     image = love.graphics.newImage('image.png')
 end
 
 function love.update(dt)
-        if _G.x < 20 then
-            _G.x = _G.x + 1 + (15 * dt)
-        end
-        if _G.y < 20 then
-            _G.y = _G.y + 1 + (15 * dt)
-        end
-        _G.x = _G.x + 1
-        _G.y = _G.y + 1
-        if _G.x > 800 then
-            _G.x = 0
-            duckNumber = 0
-            love.graphics.clear(love.graphics.getBackgroundColor())
-        end
+    frame = frame + 1
+    print(frame)
+    if _G.x < 20 then
+        _G.x = _G.x + 1 + (15 * dt)
+    end
+    if _G.y < 20 then
+        _G.y = _G.y + 1 + (15 * dt)
+    end
+    _G.x = _G.x + 1
+    _G.y = _G.y + 1
+    if _G.x > 800 then
+        _G.x = 0
+        duckNumber = 0
+        love.graphics.clear(love.graphics.getBackgroundColor())
+    end
 
-        if _G.y > 600 then
-            _G.y = 0
-            love.graphics.clear(love.graphics.getBackgroundColor())
-        end
+    if _G.y > 600 then
+        _G.y = 0
+        love.graphics.clear(love.graphics.getBackgroundColor())
+    end
 end
 
 function love.keypressed(key)
@@ -41,25 +45,44 @@ function love.keypressed(key)
 end
 
 function love.draw()
-        love.graphics.draw(image, love.math.random(0, _G.x), love.math.random(0, _G.y))
-        duckNumber = duckNumber + 1
-        love.window.setTitle("Whack-a-duck count: " .. tostring(duckNumber))
+    love.graphics.draw(image, love.math.random(0, _G.x), love.math.random(0, _G.y))
+    duckNumber = duckNumber + 1
+    love.window.setTitle("Whack-a-duck count: " .. tostring(duckNumber) .. " FPS: " .. tostring(frame))
 end
-
 
 function love.run()
     if love.math then love.math.setRandomSeed(os.time()) end
 
     if love.load then love.load(arg) end
+    print(arg[3])
+
+    -- Fixed Delta time
+    --_G.dt = 1/60
+
+    -- Variable Delta time (same as default love.run)
+    _G.dt = 0
 
     -- We don't want the first frame's dt to include time taken b_G.y love.load.
-    if love.timer then love.timer.step() end
+    if love.timer then
+        love.timer.step()
+        dt = love.timer.getDelta()
+    end
 
-    _G.dt = 0
+    -- Variable Semi-fixed TImesteep
+    -- local dt = 0
+    -- upper_dt = 1/60
+    --[[
+        while _G.dt > 0 do
+            local current_dt = math.min(dt, upper_dt)
+            if love.update then love.update(current_dt) end
+            dt = dt - current_dt
+        end
+    ]]
+
     local consecutiveLargeDts = 0
-    -- Main loop time.
     local nextTime = 0
 
+    -- Main loop time.
     while true do
         -- Process events.
         love.event.pump()
@@ -72,33 +95,35 @@ function love.run()
             love.handlers[name](a, b, c, d, e, f)
         end
 
-	 -- Update dt, as we'll be passing it to update
-     nextTime = nextTime + 1 / 60
-     if love.timer then
-         love.timer.step()
-         dt = love.timer.getDelta()
-         if dt > 0.5 and consecutiveLargeDts < 3 then
-             -- We prefer the game to slow down on large short spikes
-             -- so the units don't teleport around
-             dt = 0.016
-             consecutiveLargeDts = consecutiveLargeDts + 1
-         elseif dt <= 0.5 then
-             consecutiveLargeDts = 0
-         end
-     end
-     if _G.paused then
-         _G.dt = 0
-     end
+        -- Update dt, as we'll be passing it to update
+        nextTime = nextTime + 1 / 60
+        if love.timer then
+            love.timer.step()
+            dt = love.timer.getDelta()
+            if dt > 0.5 and consecutiveLargeDts < 3 then
+                -- We prefer the game to slow down on large short spikes
+                -- so the units don't teleport around
+                dt = 0.016
+                consecutiveLargeDts = consecutiveLargeDts + 1
+            elseif dt <= 0.5 then
+                consecutiveLargeDts = 0
+            end
+        end
+        if _G.paused then
+            _G.dt = 0
+        end
 
-	-- Call update and draw
-	if love.update and _G.paused == false then love.update(_G.dt) end -- will pass 0 if love.timer is disabled
+        -- Call update and draw
+        if love.update and _G.paused == false then love.update(_G.dt) end -- will pass 0 if love.timer is disabled
 
-	if love.graphics and love.graphics.isActive() then
-	    love.graphics.origin()
-            if love.draw and _G.paused == false then love.draw() end
-	    love.graphics.present()
-	end
+        if love.graphics and love.graphics.isActive() then
+            love.graphics.origin()
+            if love.draw and _G.paused == false then
+                love.draw()
+            end
+            love.graphics.present()
+        end
 
-	if love.timer then love.timer.sleep(0.001) end
+        if love.timer then love.timer.sleep(0.001) end
     end
 end
