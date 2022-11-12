d3c0da6ae084cbc04223088560ff1b87352d9e46
andreiserbanrazvan <andreiserbanrazvan@gmail.com>
2022-11-12 13:57:32 +0200

    pause the game count the ducks you created

--- main.lua
+++ main.lua
@@ -1,31 +1,53 @@
-x = 0
-y = 0
+if pcall(require, "lldebugger") then require("lldebugger").start() end
+if pcall(require, "mobdebug") then require("mobdebug").start() end
+
+_G.x = 0
+_G.y = 0
+_G.paused = false
+local duckNumber = 0
 function love.load()
     image = love.graphics.newImage('image.png')
 end
 
 function love.update(dt)
-    if x < 20 then
-        x = x + 1 + (15 * dt)
-    end
-    if y < 20 then
-        y = y + 1 + (15 * dt)
-    end
-    x = x + 1
-    y = y + 1
-    if x > 800 then
-        x = 0
-        love.graphics.clear(love.graphics.getBackgroundColor())
-    end
+    if _G.paused == false then
+        if _G.x < 20 then
+            _G.x = _G.x + 1 + (15 * dt)
+        end
+        if _G.y < 20 then
+            _G.y = _G.y + 1 + (15 * dt)
+        end
+        _G.x = _G.x + 1
+        _G.y = _G.y + 1
+        if _G.x > 800 then
+            _G.x = 0
+            duckNumber = 0
+            love.graphics.clear(love.graphics.getBackgroundColor())
+        end
 
-    if y > 600 then
-        y = 0
+        if _G.y > 600 then
+            _G.y = 0
+            love.graphics.clear(love.graphics.getBackgroundColor())
+        end
     end
+end
 
+function love.keypressed(key)
+    if key == "escape" then
+        if _G.paused == true then
+            _G.paused = false
+        else
+            _G.paused = true
+        end
+    end
 end
 
 function love.draw()
-    love.graphics.draw(image, love.math.random(0, x), love.math.random(0, y))
+    if _G.paused == false then
+        love.graphics.draw(image, love.math.random(0, _G.x), love.math.random(0, _G.y))
+        duckNumber = duckNumber + 1
+        love.window.setTitle("Whack-a-duck count: " .. tostring(duckNumber))
+    end
 end
 
 
@@ -36,31 +58,43 @@ function love.run()
 
     if love.load then love.load(arg) end
 
-    -- We don't want the first frame's dt to include time taken by love.load.
+    -- We don't want the first frame's dt to include time taken b_G.y love.load.
     if love.timer then love.timer.step() end
 
-    local dt = 0
-
+    _G.dt = 0
+    local consecutiveLargeDts = 0
     -- Main loop time.
+    local nextTime = 0
+
     while true do
         -- Process events.
-        if love.event then
-	    love.event.pump()
-	    for name, a,b,c,d,e,f in love.event.poll() do
-	        if name == "quit" then
-		    if not love.quit or not love.quit() then
-		        return a
-		    end
-	        end
-		love.handlers[name](a,b,c,d,e,f)
-	    end
+        love.event.pump()
+        for name, a, b, c, d, e, f in love.event.poll() do
+            if name == "quit" then
+                if not love.quit or not love.quit() then
+                    return a
+                end
+            end
+            love.handlers[name](a, b, c, d, e, f)
         end
 
-	-- Update dt, as we'll be passing it to update
-	if love.timer then
-	    love.timer.step()
-	    dt = love.timer.getDelta()
-	end
+	 -- Update dt, as we'll be passing it to update
+     nextTime = nextTime + 1 / 60
+     if love.timer then
+         love.timer.step()
+         dt = love.timer.getDelta()
+         if dt > 0.5 and consecutiveLargeDts < 3 then
+             -- We prefer the game to slow down on large short spikes
+             -- so the units don't teleport around
+             dt = 0.016
+             consecutiveLargeDts = consecutiveLargeDts + 1
+         elseif dt <= 0.5 then
+             consecutiveLargeDts = 0
+         end
+     end
+     if _G.paused then
+         _G.dt = 0
+     end
 
 	-- Call update and draw
 	if love.update then love.update(dt) end -- will pass 0 if love.timer is disabled
